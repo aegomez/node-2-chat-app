1fa12429bb104c983515dff5e79231660658fdeb
Adrian Gomez <aegomez@tekmexico.com>
2019-08-19 16:36:48 -0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,11 +15,23 @@ app.use(express.static(publicPath));
 
 io.on('connection', socket => {
   console.log('New user connected');
+  
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'The Admin welcomes you to the chat app',
+    date: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'A new user appeared!',
+    date: new Date().getTime()
+  })
 
   socket.on('createMessage', message => {
     console.log('createMessage', message);
-    // emit to every single connection
-    io.emit('newMessage', {
+    
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       date: new Date().getTime()
