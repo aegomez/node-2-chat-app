9ade11a455f4fa5fc11c024872ad7f8ec122ba15
Adrian Gomez <aegomez@tekmexico.com>
2019-08-23 16:50:17 -0700

    Change default query to getElementById

--- src/public/js/utils.ts
+++ src/public/js/utils.ts
@@ -1,3 +1,6 @@
 const $ = function(query: string, context?: Element) {
+  if (/^#\S+$/.test(query)) {
+    return document.getElementById(query.replace('#', '')) as Element;
+  }
   return (context || document).querySelector(query) as Element;
 };
